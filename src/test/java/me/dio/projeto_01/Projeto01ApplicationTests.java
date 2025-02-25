7a47a9e2461ec77ebcaab57ef81e190274477954
Isabelle Braz <pih_braz@hotmail.com>
2025-02-25 12:15:50 -0300

    UP 0.0.1

---
+++ src/test/java/me/dio/projeto_01/Projeto01ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.projeto_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
