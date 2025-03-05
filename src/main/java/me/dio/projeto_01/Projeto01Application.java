f540d8664fbb8aaa5a05718e8ad1d6c0c73e0810
Isabelle Braz <pih_braz@hotmail.com>
2025-03-05 19:43:37 -0300

    UP 0.0.5 // Subindo o projeto para o Deploy Postgree no Railway

--- src/main/java/me/dio/projeto_01/Projeto01Application.java
+++ src/main/java/me/dio/projeto_01/Projeto01Application.java
@@ -3,11 +3,12 @@ package me.dio.projeto_01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication (scanBasePackages ="me.dio")
 public class Projeto01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Projeto01Application.class, args);
+
 	}
 
 }
