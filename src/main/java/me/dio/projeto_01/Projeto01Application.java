f94634c1d005df1c5b0bd30621dd6188001a887f
Isabelle Braz <pih_braz@hotmail.com>
2025-03-12 16:13:13 -0300

    UP 0.0.6 // Subindo o projeto para o Deploy Postgree no Railway e fazendo algumas alterações

--- src/main/java/me/dio/projeto_01/Projeto01Application.java
+++ src/main/java/me/dio/projeto_01/Projeto01Application.java
@@ -3,12 +3,16 @@ package me.dio.projeto_01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication (scanBasePackages ="me.dio")
+@SpringBootApplication
 public class Projeto01Application {
-
 	public static void main(String[] args) {
+		// Debug das variáveis de ambiente
+		System.out.println("PGHOST: " + System.getenv("PGHOST"));
+		System.out.println("PGPORT: " + System.getenv("PGPORT"));
+		System.out.println("PGDATABASE: " + System.getenv("PGDATABASE"));
+		System.out.println("PGUSER: " + System.getenv("PGUSER"));
+		System.out.println("PGPASSWORD: " + System.getenv("PGPASSWORD"));
+
 		SpringApplication.run(Projeto01Application.class, args);
-
 	}
-
 }
